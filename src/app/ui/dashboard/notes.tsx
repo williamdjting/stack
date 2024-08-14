ab575fef95ee611e46fe1309d943355398deca2d
William Ting <williamdjting@gmail.com>
2024-08-13 17:15:20 -0700

    changed to notes1 supabase table

--- src/app/ui/dashboard/notes.tsx
+++ src/app/ui/dashboard/notes.tsx
@@ -4,7 +4,7 @@ import { createClient } from '/src/app/lib/supabase/server';
 
 export async function Notes() {
   const supabase = createClient();
-  const { data: notes } = await supabase.from("notes").select();
+  const { data: notes1 } = await supabase.from("notes1").select();
 
-  return <pre>{JSON.stringify(notes, null, 2)}</pre>
+  return <pre>{JSON.stringify(notes1, null, 2)}</pre>
 }
