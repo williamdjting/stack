80bf29d7a2408a8f5b5ad3902a3d39dbfae65642
William Ting <williamdjting@gmail.com>
2024-08-17 01:22:18 -0700

    added insertNote, insertNoteAPI functions to insert into DB, not yet working

--- src/app/ui/dashboard/notes.tsx
+++ src/app/ui/dashboard/notes.tsx
@@ -1,10 +1,15 @@
-`use server`
+
+// 1
+
+"use server"
 
 import { createClient } from '/src/app/lib/supabase/server';
 
 export async function Notes() {
   const supabase = createClient();
-  const { data: notes1 } = await supabase.from("notes1").select();
+  const { data: notes1 } = await supabase.from("notes2").select();
 
   return <pre>{JSON.stringify(notes1, null, 2)}</pre>
 }
+
+
