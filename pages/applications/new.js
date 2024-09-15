af508da3f5de72ec29c560e79f60a373cae11907
William Ting <williamdjting@gmail.com>
2024-09-14 23:05:44 -0700

    added new.js route for new applications

---
+++ pages/applications/new.js
@@ -0,0 +1,25 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+import { supabase } from "@/app/lib/supabase/server";
+
+//This is the new application page that will insert into the Supabase Notes1 DB
+
+const NewApplication = () => {
+
+
+
+
+
+  return (
+    <>
+      This is the new application page that will insert into the Supabase Notes1 DB
+    
+    
+    
+    </>
+
+  )
+}
+
+export default NewApplication
