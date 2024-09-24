6c57a4ab0cccb361e0a577021b33933e1df3d219
William Ting <williamdjting@gmail.com>
2024-09-24 14:51:49 -0700

    add openai folder in lib

---
+++ src/app/lib/openai/openai.ts
@@ -0,0 +1,20 @@
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
+
+const openAIApiKey: string = process.env.NEXT_PUBLIC_OPENAI_API_KEY || "";
+
+if (!openAIApiKey) {
+  throw new Error("OpenAI API key not found.");
+}
+
+export const llm = new ChatOpenAI({
+  openAIApiKey,
+  modelName: "gpt-4o-mini",
+  temperature: 0.9,
+});
+
+export const embeddings = new OpenAIEmbeddings(
+  {
+    openAIApiKey,
+  },
+  { maxRetries: 0 }
+);
