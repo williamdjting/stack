1e12a74e438fb485a00a78935d1cc46f77af8506
William Ting <williamdjting@gmail.com>
2024-09-26 14:42:02 -0700

    added ai feature given this https://js.langchain.com/docs/how_to/structured_output/

--- pages/openai/test.js
+++ pages/openai/test.js
@@ -1,10 +1,34 @@
 import { useEffect } from "react";
 
+import { z } from "zod";
+
+import { llm } from "@/app/lib/openai/openai"
+
 const AIpage = ({}) => {
 
+  const joke = z.object({
+    setup: z.string().describe("The setup of the joke"),
+    punchline: z.string().describe("The punchline to the joke"),
+    rating: z.number().optional().describe("How funny the joke is, from 1 to 10"),
+  });
   
+  const structuredLlm = llm.withStructuredOutput(joke);
+
+
   useEffect(() => {
     // Add your side effects here
+
+    const fetchData = async () => {
+      try {
+        const returnVal = await structuredLlm.invoke("Tell me a joke about cats");
+        console.log("This is the returnVal in test.js", returnVal);
+      } catch (error) {
+        console.error("Error while invoking structuredLlm", error);
+      }
+    };
+
+    fetchData(); // Call the async function
+
   }, []);
 
   return (
--- src/app/lib/openai/openai.ts
+++ src/app/lib/openai/openai.ts
@@ -1,12 +1,19 @@
 import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
 
-const openAIApiKey: string = process.env.NEXT_PUBLIC_OPENAI_API_KEY || "";
+// import dotenv from "dotenv";
+
+// // Load environment variables
+// dotenv.config();
+
+
+const openAIApiKey = process.env.NEXT_PUBLIC_OPENAI_API_KEY || "";
 
 if (!openAIApiKey) {
   throw new Error("OpenAI API key not found.");
 }
 
 export const llm = new ChatOpenAI({
+  openAIApiKey,
   modelName: "gpt-4o-mini",
   temperature: 0,
 });
