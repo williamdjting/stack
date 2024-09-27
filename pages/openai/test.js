d304c70a18714ac727ed57c0562c109c682d5146
William Ting <williamdjting@gmail.com>
2024-09-26 22:38:44 -0700

    got the langchain ai and zod to work tentatively

--- pages/openai/test.js
+++ pages/openai/test.js
@@ -4,15 +4,29 @@ import { z } from "zod";
 
 import { llm } from "@/app/lib/openai/openai"
 
+import { completion } from "zod-gpt";
+
 const AIpage = ({}) => {
 
   const joke = z.object({
-    setup: z.string().describe("The setup of the joke"),
-    punchline: z.string().describe("The punchline to the joke"),
+    job: z.string().describe("Develop and maintain web applications"),
+    responsibilities: z.string().describe("List out four responsibilites of the job"),
     rating: z.number().optional().describe("How funny the joke is, from 1 to 10"),
   });
   
   const structuredLlm = llm.withStructuredOutput(joke);
+
+  // const jobDescriptionVal = 'Develop and maintain web applications';
+
+  // const resumeExperienceVal = '3 years at WebSolutions, developed client-side features';
+
+  // const resume = z.object({
+  //   jobDescription: z.string(),
+  //   resumeExperience: z.string(),
+  // });
+
+  // const prompt = `Create four bullet points that take the ${resumeExperienceVal} and match it with the ${jobDescriptionVal}`
+
 
 
   useEffect(() => {
@@ -20,10 +34,10 @@ const AIpage = ({}) => {
 
     const fetchData = async () => {
       try {
-        const returnVal = await structuredLlm.invoke("Tell me a joke about cats");
+        const returnVal = await structuredLlm.invoke("Given the job, list out four responsibilites of the job");
         console.log("This is the returnVal in test.js", returnVal);
       } catch (error) {
-        console.error("Error while invoking structuredLlm", error);
+        console.error("Error while invoking", error);
       }
     };
 
