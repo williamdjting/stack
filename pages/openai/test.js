3d9f03af5f4ca646ba73160578bc91247f18fe6f
William Ting <williamdjting@gmail.com>
2024-09-27 16:42:13 -0700

    updated prompts in openai directory

--- pages/openai/test.js
+++ pages/openai/test.js
@@ -13,15 +13,28 @@ const AIpage = ({}) => {
 
   const resumeExperienceVal = '3 years at WebSolutions, developed client-side features';
 
-  const resumeIntoLLM = z.object({
-    job: z.string(),
+  const resumeProjectsVal = 'Data visualization dashboard, Sales forecasting model';
+
+  const resume1 = z.object({
+    jobDescription: z.string(),
     responsibilities: z.string(),
-    rating: z.number().optional(),
+    output: z.string(),
   });
   
-  const structuredLlm = llm.withStructuredOutput(resumeIntoLLM);
+  const structuredLlm1 = llm.withStructuredOutput(resume1);
 
-  const prompt = `Given the ${jobDescriptionVal}, list out in four bullet points  given past experience: ${resumeExperienceVal}, the responsibilites of related to the job`;
+  const prompt1 = `Given the ${jobDescriptionVal} and ${resumeExperienceVal} list out four bullet points ordered by number that describe the experiences as it relates to the job description. Please ensure there are empirical measurements.`;
+
+
+  const resume2 = z.object({
+    jobDescription: z.string(),
+    projects: z.string(),
+    output: z.string(),
+  });
+  
+  const structuredLlm2 = llm.withStructuredOutput(resume1);
+
+  const prompt2 = `Given the ${jobDescriptionVal} and ${resumeProjectsVal} create four bullet points for each project from ${resumeProjectsVal}. Please ensure there are empirical measurements.`;
 
 
 
@@ -30,8 +43,11 @@ const AIpage = ({}) => {
 
     const fetchData = async () => {
       try {
-        const returnVal = await structuredLlm.invoke(`${prompt}`);
-        console.log("This is the returnVal in test.js", returnVal);
+        const returnVal1 = await structuredLlm1.invoke(`${prompt1}`);
+        console.log("This is the returnVal1 in test.js", returnVal1);
+
+        const returnVal2 = await structuredLlm2.invoke(`${prompt2}`);
+        console.log("This is the returnVal2 in test.js", returnVal2);
       } catch (error) {
         console.error("Error while invoking", error);
       }
