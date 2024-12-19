0f92a91acdb3574beaeb17a21780cd4e17d0d243
akadiric <akadiric52@gmail.com>
2024-12-18 18:36:57 -0800

    onboarding is protected

--- app/supabase/middleware.ts
+++ app/supabase/middleware.ts
@@ -37,8 +37,8 @@ export async function updateSession(request: NextRequest) {
 
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
+    !request.nextUrl.pathname.startsWith('/auth/login') &&
+    !request.nextUrl.pathname.startsWith('/auth/register')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
